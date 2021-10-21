7c777d5020717dc8479edd6c0ffd208d32528c70
vnikita <vnikita123456@gmail.com>
2021-10-22 02:58:28 +0700

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Vasilev6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lYear = new System.Windows.Forms.Label();
+            this.lMounth = new System.Windows.Forms.Label();
+            this.lDay = new System.Windows.Forms.Label();
+            this.lError = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Год";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 34);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lYear
+            // 
+            this.lYear.AutoSize = true;
+            this.lYear.Location = new System.Drawing.Point(96, 101);
+            this.lYear.Name = "lYear";
+            this.lYear.Size = new System.Drawing.Size(12, 15);
+            this.lYear.TabIndex = 5;
+            this.lYear.Text = "?";
+            // 
+            // lMounth
+            // 
+            this.lMounth.AutoSize = true;
+            this.lMounth.Location = new System.Drawing.Point(96, 116);
+            this.lMounth.Name = "lMounth";
+            this.lMounth.Size = new System.Drawing.Size(12, 15);
+            this.lMounth.TabIndex = 6;
+            this.lMounth.Text = "?";
+            // 
+            // lDay
+            // 
+            this.lDay.AutoSize = true;
+            this.lDay.Location = new System.Drawing.Point(96, 131);
+            this.lDay.Name = "lDay";
+            this.lDay.Size = new System.Drawing.Size(12, 15);
+            this.lDay.TabIndex = 7;
+            this.lDay.Text = "?";
+            // 
+            // lError
+            // 
+            this.lError.AutoSize = true;
+            this.lError.Location = new System.Drawing.Point(96, 154);
+            this.lError.Name = "lError";
+            this.lError.Size = new System.Drawing.Size(57, 15);
+            this.lError.TabIndex = 8;
+            this.lError.Text = "Ошибки:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Год: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Месяц:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "День:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 178);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lError);
+            this.Controls.Add(this.lDay);
+            this.Controls.Add(this.lMounth);
+            this.Controls.Add(this.lYear);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lYear;
+        private System.Windows.Forms.Label lMounth;
+        private System.Windows.Forms.Label lDay;
+        private System.Windows.Forms.Label lError;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vasilev6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isIntDays = int.TryParse(textBox1.Text, out int number); //Проверка на то что введено число в "Дни"
+            if (isIntDays)
+            {
+                bool visok = false;
+                bool isIntYear = int.TryParse(textBox2.Text, out int year); //Проверка на то что введено число в "Год"
+                if (isIntYear)
+                {
+                    if (year % 4 == 0) //Проверка на високосный год
+                    {
+                        if (year % 100 == 0)
+                        {
+                            if (year % 400 == 0)
+                                visok = true;
+                        }
+                        else
+                            visok = true;
+                    }
+                        
+                    if (visok) //Если високосный
+                    {
+                        if (number > 0 & number <= 366) //Проверка на числа для високосного
+                        {
+                            if (number <= 31)
+                            {
+                                lMounth.Text = "Январь";
+                                lDay.Text = number.ToString();
+                            }
+
+                            else if (number <= (31+29))
+                            {
+                                lMounth.Text = "Февраль";
+                                lDay.Text = (number - 31).ToString();
+                            }
+
+                            else if (number <= (31 + 29+31))
+                            {
+                                lMounth.Text = "Март";
+                                lDay.Text = (number - (31+29)).ToString();
+                            }
+
+                            else if (number <= (31 + 29+31+30))
+                            {
+                                lMounth.Text = "Апрель";
+                                lDay.Text = (number - (31+29+31)).ToString();
+                            }
+
+                            else if (number <= (31+29+31+30+31))
+                            {
+                                lMounth.Text = "Май";
+                                lDay.Text = (number - (31 + 29 + 31+30)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31+30))
+                            {
+                                lMounth.Text = "Июнь";
+                                lDay.Text = (number - (31+ 29 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31 + 30+31))
+                            {
+                                lMounth.Text = "Июль";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31+30)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31 + 30 + 31+31))
+                            {
+                                lMounth.Text = "Август";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31+30))
+                            {
+                                lMounth.Text = "Сентябрь";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30+31))
+                            {
+                                lMounth.Text = "Октябрь";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30)).ToString();
+                            }
+
+                            else if (number <= (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31+30))
+                            {
+                                lMounth.Text = "Ноябрь";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else
+                            {
+                                lMounth.Text = "Декабрь";
+                                lDay.Text = (number - (31 + 29 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31+30)).ToString();
+                            }
+                            lYear.Text = "Високосный";
+                        }
+                        else
+                        {
+                            lError.Text = "Число несоответствует";
+                        }
+                    }
+                    else
+                    {
+                        if (number > 0 & number <= 365) //Проверка на числа для високосного
+                        {
+                            if (number <= 31)
+                            {
+                                lMounth.Text = "Январь";
+                                lDay.Text = number.ToString();
+                            }
+
+                            else if (number <= (31 + 28))
+                            {
+                                lMounth.Text = "Февраль";
+                                lDay.Text = (number - 31).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31))
+                            {
+                                lMounth.Text = "Март";
+                                lDay.Text = (number - (31 + 28)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30))
+                            {
+                                lMounth.Text = "Апрель";
+                                lDay.Text = (number - (31 + 28 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31))
+                            {
+                                lMounth.Text = "Май";
+                                lDay.Text = (number - (31 + 28 + 31 + 30)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30))
+                            {
+                                lMounth.Text = "Июнь";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30 + 31))
+                            {
+                                lMounth.Text = "Июль";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31))
+                            {
+                                lMounth.Text = "Август";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30))
+                            {
+                                lMounth.Text = "Сентябрь";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31))
+                            {
+                                lMounth.Text = "Октябрь";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30)).ToString();
+                            }
+
+                            else if (number <= (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30))
+                            {
+                                lMounth.Text = "Ноябрь";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31)).ToString();
+                            }
+
+                            else
+                            {
+                                lMounth.Text = "Декабрь";
+                                lDay.Text = (number - (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30)).ToString();
+                            }
+                            lYear.Text = "Не високосный";
+                        }
+                        else
+                        {
+                            lError.Text = "Число несоответствует";
+                        }
+                    }
+                }
+                else
+                {
+                    lError.Text = "Неправильно введен год";
+                }
+            }
+            else
+            {
+                lError.Text = "Вы ввели не число";
+            }
+        }
+    }
+}
